9af427292cab8b43ed934b5a8bdb67db54a5e89a
Oren Leavitt <oleavitt@ix.netcom.com>
2022-12-07 00:22:41 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Oren Leavitt on 12/7/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
